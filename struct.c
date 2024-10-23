73e953049b8ac8982926128cb0052e5e4e43bacf
Benjamin_C <benjamin_c@qq.com>
2024-10-23 19:02:12 +0800

    位域学习

--- struct.c
+++ struct.c
@@ -33,6 +33,45 @@ union Data  //定义共用体
     char string[25];
 };
 
+struct 
+{
+    unsigned int widthValidated;
+    unsigned int heightValidated;
+}status1;
+struct 
+{
+    unsigned int widthValidated:1;
+    unsigned int hieghtValidated:1;
+}status2;
+
+struct bit
+{
+    unsigned int age:3;    //位域的定义和使用与结构体相仿
+}Age;
+
+struct example1
+{
+    int a:4;
+    int b:5;
+    int c:7;
+};
+
+struct empty
+{
+    int a:4;
+    int  :4;//空域  /*位域可以是无名位域，这时它只用来作填充或调整位置,不能使用*/
+    int b:4; /*一个位域存储在同一个字节中，如一个字节所剩空间不够存放另一位域时，则会从下一单元起存放该位域。也可以有意使某位域从下一单元开始*/
+    int c:4;
+    /*在这个位域定义中，a 占第一字节的 4 位，后 4 位填 0 表示不使用，b 从第二字节开始，占用 4 位，c 占用 4 位*/
+};
+
+struct exmaple2
+{
+    unsigned a:1;
+    unsigned b:3;
+    unsigned c:4;
+}bit,*pbit;
+
 int main()
 {
     printf("Book1 title: %s\nBook1 author: %s\nBook1 subject: %s\nBook1 bookID: %d\n",book1.title,book1.author,book1.subject,book1.bookID);
@@ -93,6 +132,35 @@ int main()
     printf( "data.str : %s\n", data.string);
     /*这次我们在同一时间只使用一个变量，这也演示了使用共用体的主要目的*/
     /*以上代码在编译并执行后所有的成员都能完好输出，因为同一时间只用到一个成员*/
+
+    printf( "Memory size occupied by status1 : %d\n", sizeof(status1));
+    /*正常情况下，这种结构需要 8 字节的内存空间*/
+    printf( "Memory size occupied by status2 : %d\n", sizeof(status2));
+    /*现在，status 变量将占用 4 个字节的内存空间，但是只有 2 位被用来存储值*/
+
+    Age.age = 4;
+    printf( "Sizeof( Age ) : %d\n", sizeof(Age) );
+    printf( "Age.age : %d\n", Age.age );
+    Age.age = 7;
+    printf( "Age.age : %d\n", Age.age );
+    Age.age = 8; // 二进制表示为 1000 有四位，超出，执行结果为0
+    printf( "Age.age : %d\n", Age.age );
+
+    struct example1 eg1;
+    printf("Size of example1: %lu bytes\n", sizeof(eg1));
+    /*example1 结构体包含三个位域成员 a，b 和 c，它们分别占用 4 位、5 位和 7 位*/
+
+    //struct example2 bit;
+    //struct example2 *pbit;
+    bit.a=1;    /*给位域赋值（应注意赋值不能超过该位域的允许范围）*/
+    bit.b=7;    /*给位域赋值（应注意赋值不能超过该位域的允许范围）*/
+    bit.c=15;    /*给位域赋值（应注意赋值不能超过该位域的允许范围）*/
+    printf("%d,%d,%d\n",bit.a,bit.b,bit.c);    /*以整型量格式输出三个域的内容*/
+    pbit=&bit;    /*把位域变量 bit 的地址送给指针变量 pbit*/
+    pbit->a=0;    /*用指针方式给位域 a 重新赋值，赋为 0*/
+    pbit->b&=3;    /*使用了复合的位运算符 "&="，相当于：pbit->b=pbit->b&3，位域 b 中原有值为 7，与 3 作按位与运算的结果为 3（111&011=011，十进制值为 3）*/
+    pbit->c|=1;    /*使用了复合位运算符"|="，相当于：pbit->c=pbit->c|1，其结果为 15*/
+    printf("%d,%d,%d\n",pbit->a,pbit->b,pbit->c);    /*用指针方式输出了这三个域的值*/
 
     return 0;
 }
