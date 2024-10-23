bb43f530aa4131a2b32a2eb9f9a80245ce87dc0e
Benjamin_C <benjamin_c@qq.com>
2024-10-23 21:28:52 +0800

    文件操作函数

---
+++ fileFunc/file.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<winsock2.h>
+
+int main()
+{
+    FILE *fp=NULL;
+    fp=fopen("E:/VScodeProgram/LearningCode/C/fileFunc/test1.txt","w+");
+    fprintf(fp,"This is testing for fprintf...\n");
+    fputs("This is testing for fprintf...\n",fp);
+    fclose(fp);
+    /*mode（访问模式）：w（重新写入），r（读取），a（追加写入），w+（重新读写），r+（读写），a+（从头读取，追加写入）*/
+
+    FILE *fp1=NULL;
+    char string[101];
+    fp1=fopen("E:/VScodeProgram/LearningCode/C/fileFunc/test2.txt","r");
+    fscanf(fp1,"%s",string);
+    printf("1.%s\n",string);
+    fgets(string,101,(FILE*)fp1);
+    printf("2.%s\n",string);
+    fgets(string,101,(FILE*)fp1);
+    printf("3.%s\n",string);
+    fclose(fp1);
+    /*fscanf() 方法只读取了 This，因为它在后边遇到了一个空格。其次，调用 fgets() 读取剩余的部分，直到行尾。最后，调用 fgets() 完整地读取第二行*/
+
+    return 0;
+}
