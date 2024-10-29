9c7973869a3f16b365691bdc1f25663c0b6dbc9b
Benjamin_C <benjamin_c@qq.com>
2024-10-29 11:26:03 +0800

    update example_4

--- example/Example_6.c
+++ example/Example_6.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
 /*用*号输出字母C的图案*/
 int main()
-{
+{   /*方案<1>*/
     printf("*******\n");
     printf("*\n");
     printf("*\n");
     printf("*******\n");
+
+    /*方案<2>*/
+    const char *c_pattern[] = {
+        "*******",
+        "*",
+        "*",
+        "*******"
+    };
+    // 输出字符图形
+    for (int i = 0; i < sizeof(c_pattern) / sizeof(c_pattern[0]); i++) {
+        printf("%s\n", c_pattern[i]);
+    }
 
     return 0;
 }
