57e687f3b086e9d03db783f547c5557e9c5a4e5e
Benjamin_C <benjamin_c@qq.com>
2024-11-24 14:14:03 +0800

    update primeNum.c

--- NormalScript/PrimeNum.c
+++ NormalScript/PrimeNum.c
@@ -9,7 +9,7 @@ int main()
             int flag=1;
             int i;
 
-            for(i=2;i<sqrt(a);i++){  //2为最小的素数，故从2开始循环
+            for(i=2;i<sqrt(a)+1;i++){  //2为最小的素数，故从2开始循环
                 if(a%i==0){          //从0开始会出现算法错误，无法除以0
                     flag=0;
                     break;
